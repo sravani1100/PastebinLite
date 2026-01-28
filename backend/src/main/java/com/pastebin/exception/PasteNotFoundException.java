ea13ca45fea2c39bddcfe61a3d91c8886f78c525
Sravani Gangireddy <sravanigangireddy11@gmail.com>
2026-01-29 01:05:11 +0530

    Clean backend submodule traces

--- backend/src/main/java/com/pastebin/exception/PasteNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.pastebin.exception;
-
-public class PasteNotFoundException extends RuntimeException {
-
-    public PasteNotFoundException(String id) {
-        super("Paste not found with id: " + id);
-    }
-}
